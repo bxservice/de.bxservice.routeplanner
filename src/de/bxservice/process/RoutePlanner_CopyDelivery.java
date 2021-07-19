4eee12600a8e458c96a65184ef3a7936bd862c1c
Diego Ruiz <ruizg.diego@gmail.com>
2021-07-19 18:01:15 +0200

    Add date as parameter to copy deliveries

--- src/de/bxservice/process/RoutePlanner_CopyDelivery.java
+++ src/de/bxservice/process/RoutePlanner_CopyDelivery.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.compiere.model.MResourceUnAvailable;
 import org.compiere.model.Query;
 import org.compiere.model.X_C_NonBusinessDay;
+import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
 import org.compiere.util.Env;
 import org.compiere.util.TimeUtil;
@@ -15,10 +16,21 @@ import de.bxservice.model.MDelivery;
 
 public class RoutePlanner_CopyDelivery extends SvrProcess {
 
-	private Timestamp currentDay;
+	private Timestamp targetDay;
+	
+	private Timestamp p_TargetDate;
 	
 	@Override
 	protected void prepare() {
+		ProcessInfoParameter[] para = getParameter();
+		for (int i = 0; i < para.length; i++)
+		{
+			String name = para[i].getParameterName();
+			if (para[i].getParameter() == null)
+				;
+			else if (name.equals("BAY_RouteDate"))
+				p_TargetDate = para[i].getParameterAsTimestamp();
+		}
 	}
 
 	@Override
@@ -26,19 +38,23 @@ public class RoutePlanner_CopyDelivery extends SvrProcess {
 		log.warning("Copying last date deliveries");
 		
 		//Check if working day - not weekends nor holidays
-		currentDay = TimeUtil.trunc(Env.getContextAsDate(getCtx(), "#Date"), TimeUtil.TRUNC_DAY);
+		if (p_TargetDate == null)
+			p_TargetDate = Env.getContextAsDate(getCtx(), "#Date");
+
+		targetDay = TimeUtil.trunc(p_TargetDate, TimeUtil.TRUNC_DAY);
+			
 		X_C_NonBusinessDay nonWorkingDays = new Query(Env.getCtx(), X_C_NonBusinessDay.Table_Name, 
 				"AD_Client_ID IN (0, ?) AND TRUNC(" + 
 						X_C_NonBusinessDay.COLUMNNAME_Date1 + ")=? ", get_TrxName())
-				.setParameters(new Object[]{Env.getAD_Client_ID(Env.getCtx()), currentDay})
+				.setParameters(new Object[]{Env.getAD_Client_ID(Env.getCtx()), targetDay})
 				.setOnlyActiveRecords(true)
 				.first();
 		
 		if (nonWorkingDays != null)
-			return "No deliveries for non-working days: " + currentDay;
+			return "No deliveries for non-working days: " + targetDay;
 		
-		if (currentDay.equals(TimeUtil.trunc(MDelivery.getLastDeliveryDate(get_TrxName()), TimeUtil.TRUNC_DAY)))
-			return "Delivery records already created for " + currentDay;
+		if (targetDay.equals(TimeUtil.trunc(MDelivery.getLastDeliveryDate(get_TrxName()), TimeUtil.TRUNC_DAY)))
+			return "Delivery records already created for " + targetDay;
 
 		List<MDelivery> lastDeliveries = MDelivery.getLastDateDeliveries();
 		
@@ -52,12 +68,12 @@ public class RoutePlanner_CopyDelivery extends SvrProcess {
 			counter ++;
 			StringBuilder message = new StringBuilder("Delivery created");
 			MDelivery newDelivery = MDelivery.copyDelivery(delivery, get_TrxName());
-			newDelivery.setBAY_RouteDate(currentDay);
+			newDelivery.setBAY_RouteDate(targetDay);
 			if (!checkResourceAvailability(newDelivery))
 				message.append(" with unavailable resources. Please check");
 			newDelivery.saveEx(get_TrxName());
 			addLog(getProcessInfo().getAD_Process_ID(), 
-					currentDay, new BigDecimal(getProcessInfo().getAD_PInstance_ID()), 
+					targetDay, new BigDecimal(getProcessInfo().getAD_PInstance_ID()), 
 					message.toString(), MDelivery.Table_ID, 
 					newDelivery.getBAY_Delivery_ID());
 		}
@@ -69,17 +85,17 @@ public class RoutePlanner_CopyDelivery extends SvrProcess {
 		
 		boolean allResourcesAvailable = true;
 		
-		if (delivery.getBAY_Driver() != null && MResourceUnAvailable.isUnAvailable(delivery.getBAY_Driver(), currentDay)) {
+		if (delivery.getBAY_Driver() != null && MResourceUnAvailable.isUnAvailable(delivery.getBAY_Driver(), targetDay)) {
 			delivery.setBAY_Driver_ID(0);
 			allResourcesAvailable = false;
 		}
 		
-		if (delivery.getBAY_CoDriver() != null && MResourceUnAvailable.isUnAvailable(delivery.getBAY_CoDriver(), currentDay)) {
+		if (delivery.getBAY_CoDriver() != null && MResourceUnAvailable.isUnAvailable(delivery.getBAY_CoDriver(), targetDay)) {
 			delivery.setBAY_CoDriver_ID(0);
 			allResourcesAvailable = false;
 		}
 		
-		if (delivery.getBAY_Truck() != null && MResourceUnAvailable.isUnAvailable(delivery.getBAY_Truck(), currentDay)) {
+		if (delivery.getBAY_Truck() != null && MResourceUnAvailable.isUnAvailable(delivery.getBAY_Truck(), targetDay)) {
 			delivery.setBAY_Truck_ID(0);
 			allResourcesAvailable = false;
 		}
